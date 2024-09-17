0a93f22831b810f39b8608bec99b6f23aca4d4e7
artur <arturiermolenko@gmail.com>
2024-09-17 22:10:59 +0200

    added get_cook function

--- plan.py
+++ plan.py
@@ -1,4 +1,39 @@
+from datetime import datetime
+from typing import List
+from worker import Worker
+
+
 class Plan:
     def __init__(self, date: str, workers_list: List[Worker]):
-        self.date = date
-        self.workes_list = workers_list
+        self.date = datetime.strptime(date, "%d-%m-%Y")
+        self.workers_list = workers_list
+        self.cook = self.get_cook()
+
+    def print_plan(self):
+        print(
+            f'''
+            Today {self.cook} is cooking.
+            '''
+        )
+
+    def get_cook(self):
+        for worker in self.workers_list:
+            if worker.cook_day == (self.date.weekday() + 1):
+                return worker
+
+
+if __name__ == "__main__":
+    artur = Worker(
+        name="Artur",
+        surname="Iermolenko",
+        start_work_time="06.30",
+        end_work_time="15.00",
+        start_pause="10.30",
+        end_pause="11.00",
+        cook_day=1
+    )
+    plan = Plan(
+        date="16-09-2024",
+        workers_list=[artur]
+    )
+    plan.print_plan()
--- worker.py
+++ worker.py
@@ -6,25 +6,16 @@ class Worker:
             start_work_time: str,
             end_work_time: str,
             start_pause: str,
-            end_pause: str
-        ):
+            end_pause: str,
+            cook_day: int | None = None
+    ):
         self.name = name
         self.surname = surname
         self.start_work_time = start_work_time
         self.end_work_time = end_work_time
         self.start_pause = start_pause
         self.end_pause = end_pause
-    
+        self.cook_day = cook_day
+
     def __str__(self):
         return f"{self.name} {self.surname}"
-
-if __name__ == "__main__":
-    artur = Worker(
-        name = "Artur",
-        surname = "Iermolenko",
-        start_work_time = "06.30",
-        end_work_time = "15.00",
-        start_pause = "10.30",
-        end_pause = "11.00"
-    )
-    print(artur)
